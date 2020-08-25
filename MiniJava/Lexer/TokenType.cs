5b6a9ddf51ce6bd835e23d0509a820ac6e935e6f
Karen Izabel Paiz Gonzalez <karenizabel1999@gmail.com>
2020-08-24 19:31:20 -0600

    palabras reservadas

--- MiniJava/Lexer/TokenType.cs
+++ MiniJava/Lexer/TokenType.cs
@@ -7,14 +7,36 @@ namespace MiniJava.Lexer
     //Todo agregar todos los tipos de tokens
     public enum TokenType
     {
-        Identifier,
-        Token_Int,
-        Token_Boolean,
+        Identifier,  
+        //PALABRAS RESERVADAS
+        Token_void,       //YA
+        Token_int,        //YA
+        Token_double,     //YA
+        Token_boolean,    //YA
+        Token_string,     //YA
+        Token_class,      //YA
+        Token_const,      //YA
+        Token_interface,  //YA
+        Token_null,       //YA
+        Token_this,       //YA
+        Token_extends,    //YA
+        Token_implements, //YA
+        Token_for,        //YA
+        Token_while,      //YA
+        Token_if,         //YA
+        Token_else,       //YA
+        Token_return,     //YA
+        Token_break,      //YA
+        Token_New,        //YA
+        Token_System,     //YA
+        Token_out,        //YA
+        Token_printlnt,   //YA
+
+        Int,
         WhiteSpace,
-        Token_reservedWord,
-        Token_Comparison,
-        Token_ComentInBlock,
-        Token_Doubles,
-        Token_Id
+        //Token_Comparison,
+        //Token_ComentInBlock,
+        //Token_Doubles,
+        //Token_Id
     }
 }
