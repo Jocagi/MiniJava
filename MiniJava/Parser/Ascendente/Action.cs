bb51400b630b897a7996adece42c2d45f15dcfa4
Jocagi <josegiron1607@gmail.com>
2020-10-12 19:14:07 -0600

    Classes para Parser Ascendente

---
+++ MiniJava/Parser/Ascendente/Action.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MiniJava.Lexer;
+
+namespace MiniJava.Parser.Ascendente
+{
+    public class Action
+    {
+        public TokenType symbol;
+        public bool esIR_A;
+        public bool esRetroceso;
+        public bool esDesplazamiento;
+
+        public Action()
+        {
+
+        }
+    }
+}
---
+++ MiniJava/Parser/Ascendente/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniJava.Parser.Ascendente
+{
+    public class State
+    {
+        public List<Action> actions = new List<Action>();
+
+        public State()
+        {
+
+        }
+    }
+}
