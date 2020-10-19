e087e3c7c071ee49f6676b380d77e4d67148baf4
Jocagi <josegiron1607@gmail.com>
2020-10-18 23:03:42 -0600

    Create IR_A

---
+++ MiniJava/Parser/Ascendente/TableGenerator/Go_To.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MiniJava.Lexer;
+
+namespace MiniJava.Parser.Ascendente.TableGenerator
+{
+    public class Go_To
+    {
+        public int StateID { get; set; }
+        public int NextStateID { get; set; }
+        public TokenType Token { get; set; }
+        public LRItem LRItem { get; set; }
+
+        public Go_To(int ID, TokenType token, int nextState, LRItem item)
+        {
+            this.StateID = ID;
+            this.Token = token;
+            this.NextStateID = nextState;
+            this.LRItem = item;
+        }
+
+        public bool isEqual(Go_To _goto)
+        {
+            return this.Token == _goto.Token;
+        }
+    }
+}
