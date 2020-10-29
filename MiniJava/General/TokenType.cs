fa8f3b505af3834c0597f9a79007677501e37015
Karen Izabel <karenizabel1999@gmail.com>
2020-10-29 02:02:36 -0600

    const -- > static

--- MiniJava/General/TokenType.cs
+++ MiniJava/General/TokenType.cs
@@ -15,7 +15,7 @@ namespace MiniJava.Lexer
         Token_boolean,     
         Token_string,      
         Token_class,       
-        Token_const,       
+        Token_static,       
         Token_interface,   
         Token_null,        
         Token_this,        
