fb8d4626f30233f43c579c39d30d9b41dc5e7000
Jocagi <josegiron1607@gmail.com>
2020-10-30 00:13:36 -0600

    Gramatica: Declaracion variable

--- MiniJava/Parser/Ascendente/TableGenerator/Grammar/GrammarDefinition.cs
+++ MiniJava/Parser/Ascendente/TableGenerator/Grammar/GrammarDefinition.cs
@@ -13,30 +13,47 @@ namespace MiniJava.Parser.Ascendente.TableGenerator.Grammar
 
         public static List<Production> productions = new List<Production>
         {
+            //Inicio
             new Production(TokenType.NT_Start, new List<TokenType>{TokenType.NT_Program}),
             new Production(TokenType.NT_Program, new List<TokenType>{TokenType.NT_Decl}),
-
+            //Declaraciones
+            new Production(TokenType.NT_Decl, new List<TokenType>{TokenType.NT_VariableDecl, TokenType.NT_Decl1}),
             new Production(TokenType.NT_Decl, new List<TokenType>{TokenType.NT_ConstDecl, TokenType.NT_Decl1}),
             new Production(TokenType.NT_Decl1, new List<TokenType>{TokenType.NT_Decl}),
             new Production(TokenType.NT_Decl1, new List<TokenType>{TokenType.Epsilon}),
-            
+            //Declaracion de constante
             new Production(TokenType.NT_ConstDecl, new List<TokenType>{TokenType.Token_static, TokenType.NT_ConstType, TokenType.Identifier, TokenType.Operator_puntoComa}),
             new Production(TokenType.NT_ConstType, new List<TokenType>{TokenType.Const_Int}),
             new Production(TokenType.NT_ConstType, new List<TokenType>{TokenType.Const_double}),
             new Production(TokenType.NT_ConstType, new List<TokenType>{TokenType.Const_bool}),
             new Production(TokenType.NT_ConstType, new List<TokenType>{TokenType.Const_String}),
+            //Declaracion de variable
+            new Production(TokenType.NT_VariableDecl, new List<TokenType>{TokenType.NT_Variable, TokenType.Operator_puntoComa}),
+            new Production(TokenType.NT_Variable, new List<TokenType>{TokenType.NT_Type, TokenType.Identifier}),
+            new Production(TokenType.NT_Type, new List<TokenType>{TokenType.Const_Int}),
+            new Production(TokenType.NT_Type, new List<TokenType>{TokenType.Const_double}),
+            new Production(TokenType.NT_Type, new List<TokenType>{TokenType.Const_bool}),
+            new Production(TokenType.NT_Type, new List<TokenType>{TokenType.Const_String}),
+            new Production(TokenType.NT_Type, new List<TokenType>{TokenType.Identifier}),
+            new Production(TokenType.NT_Type, new List<TokenType>{TokenType.NT_Type, TokenType.Operator_corchetes}),
         };
 
         public static List<First> first = new List<First>
         {
-            new First(TokenType.NT_Start, new List<TokenType>{TokenType.Token_static}),
-            new First(TokenType.NT_Program, new List<TokenType>{TokenType.Token_static}),
-
-            new First(TokenType.NT_Decl, new List<TokenType>{TokenType.Token_static}),
-            new First(TokenType.NT_Decl1, new List<TokenType>{TokenType.Token_static}),
-
+            //Inicio
+            new First(TokenType.NT_Start, new List<TokenType>{TokenType.Token_static,TokenType.Const_Int, TokenType.Const_double, TokenType.Const_bool, TokenType.Const_String, TokenType.Identifier}),
+            new First(TokenType.NT_Program, new List<TokenType>{TokenType.Token_static,TokenType.Const_Int, TokenType.Const_double, TokenType.Const_bool, TokenType.Const_String, TokenType.Identifier}),
+            //Declaraciones
+            new First(TokenType.NT_Decl, new List<TokenType>{TokenType.Token_static,TokenType.Const_Int, TokenType.Const_double, TokenType.Const_bool, TokenType.Const_String, TokenType.Identifier}),
+            new First(TokenType.NT_Decl1, new List<TokenType>{TokenType.Token_static,TokenType.Const_Int, TokenType.Const_double, TokenType.Const_bool, TokenType.Const_String, TokenType.Identifier}),
+            //Declaracion de contantes
             new First(TokenType.NT_ConstDecl, new List<TokenType>{TokenType.Token_static}),
-            new First(TokenType.NT_ConstType, new List<TokenType>{TokenType.Const_Int, TokenType.Const_double, TokenType.Const_bool, TokenType.Const_String})
+            new First(TokenType.NT_ConstType, new List<TokenType>{TokenType.Const_Int, TokenType.Const_double, TokenType.Const_bool, TokenType.Const_String}),
+            //Declaracion de variables
+            new First(TokenType.NT_VariableDecl, new List<TokenType>{TokenType.Const_Int, TokenType.Const_double, TokenType.Const_bool, TokenType.Const_String, TokenType.Identifier}),
+            new First(TokenType.NT_Variable, new List<TokenType>{TokenType.Const_Int, TokenType.Const_double, TokenType.Const_bool, TokenType.Const_String, TokenType.Identifier}),
+            new First(TokenType.NT_Type, new List<TokenType>{TokenType.Const_Int, TokenType.Const_double, TokenType.Const_bool, TokenType.Const_String, TokenType.Identifier}),
+
         };
     }
 }
