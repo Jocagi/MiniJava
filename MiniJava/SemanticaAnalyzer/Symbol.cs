6407374e91f57e76c526835cd1f9aa4eed0c6151
Jocagi <josegiron1607@gmail.com>
2020-11-20 17:50:42 -0600

    Tipos de simbolos

---
+++ MiniJava/SemanticaAnalyzer/Symbol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniJava.SemanticaAnalyzer
+{
+    public class Symbol
+    {
+        public string ID { get; set; }
+        public int scope { get; set; }
+        public int value { get; set; }
+        public SymbolType type { get; set; }
+    }
+}
---
+++ MiniJava/SemanticaAnalyzer/SymbolType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniJava.SemanticaAnalyzer
+{
+    public enum SymbolType
+    {
+        variable,
+        function
+    }
+}
